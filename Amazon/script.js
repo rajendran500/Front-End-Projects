564d6d76d5d0a60ed953970a25cfb01220191c4d
Harish Rajendran <rajendranharish563@gmail.com>
2025-05-17 17:28:27 +0530

    Add files via upload

---
+++ Amazon/script.js
@@ -0,0 +1,31 @@
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelector('.control_prev');
+const next_btn = document.querySelector('.control_next');
+let n = 0;
+
+function changeSlide() {
+    for (let i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = 'none';
+    }
+    imgs[n].style.display = 'block';
+}
+changeSlide();
+
+prev_btn.addEventListener('click', () => {
+    n = (n > 0) ? n - 1 : imgs.length - 1;
+    changeSlide();
+});
+
+next_btn.addEventListener('click', () => {
+    n = (n < imgs.length - 1) ? n + 1 : 0;
+    changeSlide();
+});
+
+const scrollContainer = document.querySelectorAll('.products');
+
+for (const item of scrollContainer) {
+    item.addEventListener('wheel', (evt) => {
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY;
+    });
+}
